9b406d6afb7c5f35a38b1fd366a10fc3f72db8ad
iNeelPatel <neelpatel.6531@gmail.com>
2020-12-07 14:32:11 +0530

    init

---
+++ precache-manifest.369bffd2818284cf90888998f1f83242.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0df2e5739804dab067052711e93beaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a761747ed2bd8ef44830",
+    "url": "/static/css/2.3107b260.chunk.css"
+  },
+  {
+    "revision": "c44e57e25565b64c24c8",
+    "url": "/static/css/main.b6f60975.chunk.css"
+  },
+  {
+    "revision": "a761747ed2bd8ef44830",
+    "url": "/static/js/2.d7f9adec.chunk.js"
+  },
+  {
+    "revision": "b3e3611e41f59548f58e6bc1d5345160",
+    "url": "/static/js/2.d7f9adec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2745b9ab0adacf07e9c3",
+    "url": "/static/js/3.99511fc0.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/static/js/3.99511fc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c44e57e25565b64c24c8",
+    "url": "/static/js/main.fa74cd82.chunk.js"
+  },
+  {
+    "revision": "8d8db718fcd48c572490",
+    "url": "/static/js/runtime-main.3bbee2e8.js"
+  },
+  {
+    "revision": "033de9526419411710ec8e50feb6bdac",
+    "url": "/static/media/exactt_logo.033de952.png"
+  },
+  {
+    "revision": "b65169148a35003a2eb1ab480f8f2fdc",
+    "url": "/static/media/exactt_w.b6516914.png"
+  },
+  {
+    "revision": "2a86ebd41aee7929f1d962ed5e52dc38",
+    "url": "/static/media/image.2a86ebd4.png"
+  },
+  {
+    "revision": "ce5d565fb0558161786a3349b0dd1aea",
+    "url": "/static/media/printer.ce5d565f.svg"
+  }
+]);
